b1cc9d081c13b713502baea0580465ac2f0e1ed9
Jayasurya R <suryasunrise261@gmail.com>
2024-12-20 23:30:27 +0530

    code updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,18 +67,21 @@ const Hero = () => {
             <FlipWords words={words} />
           </div>
         </div>
-       <Link to='/video'>
+       
        {isMobile ? (
         <button class="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce mt-10">
         ⚠️ Best viewed on a laptop or desktop for optimal experience.
       </button>
       
       ) : (
+        
+       <Link to="/video">
         <button className="group relative bg-slate-900 h-16 w-64 border-2 border-teal-600 text-white text-base font-bold rounded-xl overflow-hidden transform transition-all duration-500 hover:scale-105 hover:border-emerald-400 hover:text-emerald-300 p-3 text-left before:absolute before:w-10 before:h-10 before:content[''] before:right-2 before:top-2 before:z-10 before:bg-indigo-500 before:rounded-full before:blur-lg before:transition-all before:duration-500 after:absolute after:z-10 after:w-16 after:h-16 after:content[''] after:bg-teal-400 after:right-6 after:top-4 after:rounded-full after:blur-lg after:transition-all after:duration-500 hover:before:right-10 hover:before:-bottom-4 hover:before:blur hover:after:-right-6 hover:after:scale-110 mt-40">
           Explore More
         </button>
+        </Link>
       )}
-       </Link>
+      
       </div>
       <div className="bg-[#b8b8c2] w-full h-full">
         <AnimatedTestimonials testimonials={testimonials} />
