09e6954e2d9fdfec7d8c7c503038f920eef16c8a
Jayasurya R <suryasunrise261@gmail.com>
2025-01-07 20:35:32 +0530

    code updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -165,7 +165,7 @@ const Hero = () => {
               as="button"
               className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
             >
-              
+              Feel free to contact me!
             </CardItem>
           </div>
         </CardBody>
