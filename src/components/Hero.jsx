cc8a49e6f5ef98bfe49f48cb71df46f12301d0cc
Jayasurya R <suryasunrise261@gmail.com>
2025-01-07 20:20:55 +0530

    image profile updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -154,7 +154,7 @@ const Hero = () => {
             <CardItem
               translateZ={20}
               as={Link}
-              to="https://twitter.com/mannupaaji"
+              to=""
               target="__blank"
               className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
             >
